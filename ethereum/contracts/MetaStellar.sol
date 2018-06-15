c4574233e83d064b64dc6873b8346a932e236b17
astrocket <leonardo.lee1993@gmail.com>
2018-06-15 18:39:53 +0900

    Add notes to contract

--- ethereum/contracts/MetaStellar.sol
+++ ethereum/contracts/MetaStellar.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.24;
 
 contract MetaStellar {
 
+    // metaid
     struct MetaID {
         address owner;
         string name;
@@ -18,25 +19,28 @@ contract MetaStellar {
     }
 
     address public cosmos;
-    mapping(address => mapping(uint => uint)) public astroBucketIndex;
-    mapping(address => uint[]) public astroBucket; // ids of stars each MetaID has.
+    mapping(address => mapping(uint => uint)) public astroBucketIndex; // list of starIndexes for given address
+    mapping(address => uint[]) public astroBucket; // ids of stars for given address
     mapping(uint => Astro) public constellation; // find Astro by id(foreign key).
     uint public minimumPrice;
     uint public lastId;
     uint public multiplier;
 
+    // modifier to verify that sender is the owner of current contract
     modifier cosmosOnly() {
         require(msg.sender == cosmos);
         _;
     }
 
+    // initializer
     constructor(address creator, uint minPrice) public {
         cosmos = creator;
-        minimumPrice = minPrice;
-        lastId = 0;
+        minimumPrice = minPrice; // minimum bidding price
+        lastId = 0; // setting id index
         multiplier = 18;
     }
 
+    // registering star. 
     function registerAstro(int _raDecimal, int _decDecimal, string _name) public payable cosmosOnly {
         MetaID memory metaID = MetaID({owner : msg.sender, name : 'Metadium', sns: 'metadium.com'});
         uint newAstroId = ++lastId;
@@ -49,6 +53,7 @@ contract MetaStellar {
             lastBid : 0
             });
 
+        // allocate star into constellation with id
         constellation[newAstroId] = astro;
         addToBucket(msg.sender, newAstroId);
     }
@@ -56,7 +61,7 @@ contract MetaStellar {
     function buyAstro(uint _targetAstroId, string _metaIDName, string _metaIDSns) public payable {
         Astro storage targetAstro = constellation[_targetAstroId];
 
-        require(msg.value >= minimumPrice + targetAstro.lastBid);
+        require(msg.value >= minimumPrice + targetAstro.lastBid); // verify new bid qualifies minimum price bid gap.
 
         MetaID memory lastMetaID = targetAstro.metaID;
         MetaID memory newMetaID = MetaID({
@@ -65,18 +70,20 @@ contract MetaStellar {
             sns : _metaIDSns
             });
         targetAstro.metaID = newMetaID;
-        targetAstro.lastBid = msg.value;
-        lastMetaID.owner.transfer(msg.value);
-        removeFromBucket(lastMetaID.owner, _targetAstroId);
-        addToBucket(msg.sender, _targetAstroId);
+        targetAstro.lastBid = msg.value; // update bidding price of current star
+        lastMetaID.owner.transfer(msg.value); // refund ether to previous owner.
+        removeFromBucket(lastMetaID.owner, _targetAstroId); // remove star from previous owner's address
+        addToBucket(msg.sender, _targetAstroId); // add star to current buyer's address
     }
 
+    // function to add star id into bucket of given _hodler(address)
     function addToBucket(address _hodler, uint _astroId) internal {
         uint bucketId = astroBucket[_hodler].length;
         astroBucketIndex[_hodler][_astroId] = bucketId;
         astroBucket[_hodler].push(_astroId);
     }
 
+    // function to remove star id from bucket of given _hodler(address)
     function removeFromBucket(address _hodler, uint _astroId ) internal {
         uint bucketId = astroBucketIndex[_hodler][_astroId];
         uint bucketLength = astroBucket[_hodler].length;
@@ -87,10 +94,12 @@ contract MetaStellar {
         astroBucket[_hodler].length--;
     }
 
+    // returns length of given _hodler's bucket. -> with it, front end will use it for looping
     function getBucketLength(address _hodler) view public returns (uint length) {
         return astroBucket[_hodler].length;
     }
 
+    // returns star's information of given _id
     function getAstro(uint _id) view public returns (
         uint id,
         int raDecimal,
